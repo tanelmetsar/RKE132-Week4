ff5664ee8af53d0a30dc7de276b99d230419d1e5
tanelmetsar <tanel.metsar@tktk.ee>
2024-04-26 22:58:09 +0300

    add week4 code

---
+++ Week4/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int sum = 0;
+
+
+for (int i = 1; i < 4; i++)
+{
+    Console.WriteLine($"i = {i}");
+    Console.WriteLine($"Current total: {sum}");
+    sum = sum + i; //sum += i
+}
---
+++ Whileloops/Program.cs
@@ -0,0 +1,25 @@
+﻿Random rdn = new Random();
+int cpuRandom;
+
+bool loopActive = true; //bool - true/false
+
+while (loopActive)
+{
+    cpuRandom = rdn.Next(1, 4);
+    //Console.WriteLine($"cpu has generated {cpuRandom}");
+    Console.WriteLine("Make a guess. Enter a number 1-3 ");
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congrationlations, you won!");
+        //loopActive = false;
+        break;
+
+    }
+    else
+    {
+        Console.WriteLine("Oops, try again");
+    }
+}
+Console.WriteLine("Have a nice day!");
